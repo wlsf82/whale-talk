96cd65f931e0773a7847e23ccc75863efe9a088d
Walmyr <wlsf82@gmail.com>
2020-05-04 23:56:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# whale-talk
+
+Freedom project from the Loops section of Codecademy's Introduction to JavaScript course.
+
+```
+/* Whale language
+ * Whales speak only vowels
+ * Whales speak out and loud
+ * And when they say 'E' or 'U', they say it slowly */
+```
---
+++ whale-talk.js
@@ -0,0 +1,26 @@
+/* Whale language
+ * Whales speak only vowels
+ * Whales speak out and loud
+ * And when they say 'E' or 'U', they say it slowly */
+
+const vowels = str => {
+  str = str.toUpperCase();
+
+  const vowels = ['A', 'E', 'I', 'O', 'U'];
+
+  let letters = str.split('');
+  let vowelsFound = [];
+
+  for (let i in letters) {
+    if (vowels.includes(letters[i])) {
+      vowelsFound.push(letters[i]);
+      if(letters[i] === vowels[1] || letters[i] === vowels[4]) {
+        vowelsFound.push(letters[i]);
+      }
+    }
+  }
+
+  return vowelsFound.join('');
+};
+
+console.log(vowels('Finding Nemo')); // IIEEO
